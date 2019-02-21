cfb60644697bf5490ae9e8837ccfd0e0776d94dd
Raúl López <raullopez@siani.es>
2019-02-21 10:08:15 +0000

    Fix minor bug (metamodel)

--- applications/modeleditor/org.monet.metamodel/src/org/monet/bpi/ClientService.java
+++ applications/modeleditor/org.monet.metamodel/src/org/monet/bpi/ClientService.java
@@ -5,27 +5,29 @@ import org.monet.bpi.types.File;
 public abstract class ClientService {
 
   protected static ClientService instance;
-  
+
   public static void redirectUserTo(MonetLink monetLink) {
     instance.redirectUserToImpl(monetLink);
   }
-  
+
   public static void sendMessageToUser(String message) {
     instance.sendMessageToUserImpl(message);
   }
-  
+
   public static void sendFileToUser(File file) {
     instance.sendFileToUserImpl(file);
   }
 
-  public static User discoverUserInSession();
+  public static User discoverUserInSession() {
+    return instance.discoverUserInSessionImpl();
+  }
 
   protected abstract void redirectUserToImpl(MonetLink monetLink);
 
   protected abstract void sendMessageToUserImpl(String message);
-  
+
   protected abstract void sendFileToUserImpl(File file);
 
   protected abstract User discoverUserInSessionImpl();
-  
+
 }
