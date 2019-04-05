9c7310c4260e2f4c301bd34006ae90b350e0ba22
Raúl López <raullopez@siani.es>
2019-04-05 13:30:07 +0100

    space artifact: added version info.

--- applications/space/server/generate-version.sh
+++ applications/space/server/generate-version.sh
@@ -2,14 +2,14 @@
 
 date=`date +"%d/%m/%Y %H:%M"`
 host=`uname -a`
-
-git log | head -n 4 > version.txt
-echo "Generate WAR date: $date" >> version.txt
-echo "Generate host: $host" >> version.txt
+relative_dir=target/space/WEB-INF
+mkdir -p $relative_dir
+git log | head -n 4 > $relative_dir/version.txt
+echo "Generate WAR date: $date" >> $relative_dir/version.txt
+echo "Generate host: $host" >> $relative_dir/version.txt
 
 info=`git log | head -n 4`
 info=`echo ${info/</}`
 info=`echo ${info/>/}`
 xml="<monet-info>$info</monet-info><monet-generatedate>$date</monet-generatedate><monet-generatehost>$host</monet-generatehost>"
-echo $xml > version.xml
-
+echo $xml > $relative_dir/version.xml
