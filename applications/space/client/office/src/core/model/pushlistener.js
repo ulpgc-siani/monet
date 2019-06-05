aff809fbd0ee100bcc0845bf92b3c3b4396aab3f
Mario Caballero Ramírez <mariocaballero@siani.es>
2019-06-05 14:27:52 +0100

    Fix bug loading multiple composite inside multiple composite with field dependencies

--- applications/space/client/office/src/core/model/pushlistener.js
+++ applications/space/client/office/src/core/model/pushlistener.js
@@ -8,7 +8,7 @@ PushListener.nodeFieldFocused = function (Sender) {
 };
 
 PushListener.nodeFieldCompositeChanged = function (Sender) {
-  PushListener.resetObserverFields(Sender.DOMNode, Sender.DOMField.getCode());
+  PushListener.resetObserverFields(Sender.DOMNode, Sender.DOMField);
 };
 
 PushListener.nodeFieldChanged = function (Sender) {
@@ -19,17 +19,21 @@ PushListener.nodeFieldChanged = function (Sender) {
   Process.Data = Sender.DOMField.getContent();
   Process.execute();
 
-  PushListener.resetObserverFields(Sender.DOMNode, Sender.DOMField.getCode());
+  PushListener.resetObserverFields(Sender.DOMNode, Sender.DOMField);
 };
 
-PushListener.resetObserverFields = function (DOMElement, fieldCode) {
-  var DOMFields = DOMElement.getFields();
-  for (var i = 0; i < DOMFields.length; i++) {
-    var DOMField = DOMFields[i];
-    if (DOMField.isObserver(fieldCode))
-      DOMField.reset();
-    if (DOMField.isComposite())
-      PushListener.resetObserverFields(DOMField, fieldCode);
+PushListener.resetObserverFields = function (DOMElement, DOMSender) {
+  var DOMChildren = DOMElement.getFields();
+  var fieldCode = DOMSender.getCode();
+  for (var i = 0; i < DOMChildren.length; i++) {
+    var DOMChild = DOMChildren[i];
+    var DOMBrother = DOMChild.getBrother(fieldCode);
+    var childBrotherId = DOMBrother ? DOMBrother.id : null;
+    var sameParent = childBrotherId !== DOMChild.id && childBrotherId === DOMSender.id && DOMElement.isComposite();
+    if (sameParent && DOMChild.isObserver(fieldCode))
+      DOMChild.reset();
+    if (DOMChild.isComposite())
+      PushListener.resetObserverFields(DOMChild, DOMSender);
   }
 };
 
