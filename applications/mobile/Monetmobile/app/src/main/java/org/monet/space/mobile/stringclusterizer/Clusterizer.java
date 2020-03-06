e7913539b63419bb2358ac42d560c32943fb43c9
sfalcon <sergioafloigres@gmail.com>
2020-03-06 13:46:45 +0000

    Fixed Clusterizer

--- applications/mobile/Monetmobile/app/src/main/java/org/monet/space/mobile/stringclusterizer/Clusterizer.java
+++ applications/mobile/Monetmobile/app/src/main/java/org/monet/space/mobile/stringclusterizer/Clusterizer.java
@@ -1,137 +1,120 @@
 package org.monet.space.mobile.stringclusterizer;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public abstract class Clusterizer<T> {
-    protected final StringExtractor<T> extractor;
-    private final List<Cluster<T>> filteredLeafs = new ArrayList();
 
-    public Clusterizer() {
+    protected final StringExtractor<T> extractor;
+    private final List<Cluster<T>> filteredLeafs = new ArrayList<>();
+
+    public Clusterizer(){
         this.extractor = new StringExtractor<T>() {
+            @Override
             public String extract(T object) {
                 return object.toString();
             }
         };
     }
 
-    public Clusterizer(StringExtractor<T> extractor) {
+    public Clusterizer(StringExtractor<T> extractor){
         this.extractor = extractor;
     }
 
     @SafeVarargs
     public final ItemList<T> clusterize(T... elements) {
-        return this.clusterize(Arrays.asList(elements));
+        return clusterize(Arrays.asList(elements));
     }
 
+    private ItemList<T> removeExtraClusters(ItemList<T> clusterize) {
+        for (Item<T> item : new ArrayList<>(clusterize.edit())) removeExtraClusters(item);
+        return clusterize;
+    }
+
+    private void removeExtraClusters(Item<T> item) {
+        if(item.items().size() == 1 && item.id().equals(item.items().get(0).id())) {
+            int index = item.parent().items().edit().indexOf(item);
+            item.parent().items().edit().remove(item);
+            item.parent().items().edit().add(index, item.items().get(0));
+        } else removeExtraClusters(item.items());
+    }
+
+    @SuppressWarnings("unchecked")
     public final ItemList<T> clusterize(List<T> elements) {
-        if (elements.isEmpty()) {
-            return new ItemList();
-        } else {
-            Cluster<T> cluster = new Cluster("", elements, this.extractor);
-            this.process(cluster);
-            return cluster.items();
-        }
+        if(elements.isEmpty()) return new ItemList<>();
+        Cluster<T> cluster = new Cluster<>("", elements, extractor);
+        process(cluster);
+        return removeExtraClusters(cluster.items());
     }
 
-    private void process(Cluster<T> cluster) {
-        for(List leafClusters = this.filter(this.getLeafClusters(cluster)); !leafClusters.isEmpty(); leafClusters = this.filter(this.getLeafClusters(cluster))) {
-            this.process(leafClusters);
+    private void process(Cluster<T> cluster){
+        List<Cluster<T>> leafClusters = filter(getLeafClusters(cluster));
+        while(!leafClusters.isEmpty()) {
+            process(leafClusters);
+            leafClusters = filter(getLeafClusters(cluster));
         }
-
     }
 
     private List<Cluster<T>> filter(List<Cluster<T>> leafClusters) {
-        List<Cluster<T>> result = new ArrayList();
-        Iterator var3 = leafClusters.iterator();
-
-        while(var3.hasNext()) {
-            Cluster<T> leafCluster = (Cluster)var3.next();
-            if (!this.filteredLeafs.contains(leafCluster)) {
-                result.add(leafCluster);
-            }
+        List<Cluster<T>> result = new ArrayList<>();
+        for (Cluster<T> leafCluster : leafClusters) {
+            if(filteredLeafs.contains(leafCluster)) continue;
+            result.add(leafCluster);
         }
-
         return result;
     }
 
     private void process(List<Cluster<T>> leafClusters) {
-        Iterator var2 = leafClusters.iterator();
-
-        while(var2.hasNext()) {
-            Cluster<T> leafCluster = (Cluster)var2.next();
+        for (Cluster<T> leafCluster : leafClusters){
             String id = leafCluster.id();
-            this.clusterize(leafCluster);
-            if (id.equals(leafCluster.id()) && leafCluster.clusters().size() == 0) {
-                this.filteredLeafs.add(leafCluster);
-            }
+            clusterize(leafCluster);
+            if (!id.equals(leafCluster.id()) || leafCluster.clusters().size() != 0) continue;
+            filteredLeafs.add(leafCluster);
         }
-
     }
 
     private void clusterize(Cluster<T> cluster) {
-        Map<String, List<T>> map = this.buildClusterizationMap(cluster);
-        if (this.thereIsOnlyOneCluster(map, cluster)) {
-            this.updateClusterId(cluster, map);
-        } else {
-            this.createClusters(cluster, map);
-        }
-
+        Map<String, List<T>> map = buildClusterizationMap(cluster);
+//        if (thereIsOnlyOneCluster(map, cluster)) updateClusterId(cluster, map);
+        createClusters(cluster, map);
     }
 
-    protected abstract Map<String, List<T>> buildClusterizationMap(Cluster<T> var1);
+    protected abstract Map<String, List<T>> buildClusterizationMap(Cluster<T> cluster);
 
     private boolean thereIsOnlyOneCluster(Map<String, List<T>> map, Cluster cluster) {
-        return map.size() == 1 && ((List)map.values().iterator().next()).size() == cluster.elements().size();
+        return map.size() == 1 && map.values().iterator().next().size() == cluster.elements().size();
     }
 
     private void updateClusterId(Cluster cluster, Map<String, List<T>> map) {
-        cluster.id((String)map.keySet().iterator().next());
+        cluster.id(map.keySet().iterator().next());
     }
 
     private void createClusters(Cluster<T> cluster, Map<String, List<T>> map) {
-        Iterator var3 = map.keySet().iterator();
-
-        while(var3.hasNext()) {
-            String key = (String)var3.next();
-            if (((List)map.get(key)).size() != 1) {
-                cluster.add(new Cluster(key, (List)map.get(key), this.extractor));
-            }
+        for (String key : map.keySet()) {
+            if (map.get(key).size() == 1 && key.equals(map.get(key).get(0))) continue;
+            cluster.add(new Cluster<>(key, map.get(key), extractor));
         }
-
     }
 
     protected LinkedHashMap<String, List<T>> buildMap() {
         return new LinkedHashMap<String, List<T>>() {
+            @Override
             public List<T> get(Object key) {
-                if (!this.containsKey(key)) {
-                    this.put((String)key, new ArrayList());
-                }
-
-                return (List)super.get(key);
+                if (!containsKey(key)) put((String) key, new ArrayList<T>());
+                return super.get(key);
             }
         };
     }
 
     protected List<Cluster<T>> getLeafClusters(Cluster<T> root) {
-        List<Cluster<T>> clusters = new ArrayList();
-        Iterator var3 = root.allClusters().iterator();
-
-        while(var3.hasNext()) {
-            Cluster<T> cluster = (Cluster)var3.next();
-            if (cluster.clusters().size() <= 0) {
-                clusters.add(cluster);
-            }
+        List<Cluster<T>> clusters = new ArrayList<>();
+        for (Cluster<T> cluster : root.allClusters()) {
+            if (cluster.clusters().size() > 0) continue;
+            clusters.add(cluster);
         }
-
         return clusters;
     }
 
     protected String extract(T element) {
-        return this.extractor.extract(element);
+        return extractor.extract(element);
     }
 }
--- applications/mobile/Monetmobile/app/src/main/java/org/monet/space/mobile/viewholders/TaskGroupViewHolder.java
+++ applications/mobile/Monetmobile/app/src/main/java/org/monet/space/mobile/viewholders/TaskGroupViewHolder.java
@@ -53,7 +53,7 @@ public class TaskGroupViewHolder extends ViewHolder {
 
     public TaskGroupViewHolder group(Item group) {
         int level = calculateLevel(group);
-        title.setText(removeLabelPreviousLevel(level,group.id()));
+        title.setText(removeLabelPreviousLevel(level,group.id().replaceAll("\n","")));
         styleIndentationLevel(level);
         title.setTextColor(colorIndentationLevel(level));
         title.setTypeface(title.getTypeface(), Typeface.BOLD_ITALIC);
